755f245cbacbed687feadd7dc279f8925f751580
subhadeep1912 <37493730+subhadeep1912@users.noreply.github.com>
2021-05-08 00:47:55 +0530

    Only files uploaded

---
+++ popup.js
@@ -0,0 +1,9 @@
+theelement = "ytd-item-section-renderer"
+
+
+console.log("Chrome Extension Go!!")
+
+let rec = document.getElementById("related")
+console.log("did it work")
+rec.style.display = "none"
+// rec.hidden = true
